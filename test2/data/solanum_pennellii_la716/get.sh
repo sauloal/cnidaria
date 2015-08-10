9a2ead3d61ccd3e8685c71754c9d397b58c19f75
sauloal <sauloal@yahoo.com.br>
2015-08-10 17:33:21 +0200

    added test2 with automatic fastq analysis

---
+++ test2/data/solanum_pennellii_la716/get.sh
@@ -0,0 +1,2 @@
+wget -q -O - ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR410/ERR410247/ERR410247_1.fastq.gz | gunzip -c | head -400000 | pigz -1c > 1.fq.gz
+wget -q -O - ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR410/ERR410247/ERR410247_2.fastq.gz | gunzip -c | head -400000 | pigz -1c > 2.fq.gz
