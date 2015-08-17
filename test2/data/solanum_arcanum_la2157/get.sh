82fb68963ab94f46886801e327d70d5bf9d00b1a
sauloal <sauloal@yahoo.com.br>
2015-08-17 10:27:06 +0200

    universal makefile on test2

--- test2/data/solanum_arcanum_la2157/get.sh
+++
@@ -1,2 +0,0 @@
-wget -q -O - ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR410/ERR410227/ERR410227_1.fastq.gz | gunzip -c | head -400000 | pigz -1c > 1.fq.gz
-wget -q -O - ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR410/ERR410227/ERR410227_2.fastq.gz | gunzip -c | head -400000 | pigz -1c > 2.fq.gz
--- test2/data/solanum_habrochaites_lyc4/get.sh
+++
@@ -1,2 +0,0 @@
-wget -q -O - ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR410/ERR410237/ERR410237_1.fastq.gz | gunzip -c | head -400000 | pigz -1c > 1.fq.gz
-wget -q -O - ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR410/ERR410237/ERR410237_2.fastq.gz | gunzip -c | head -400000 | pigz -1c > 2.fq.gz
