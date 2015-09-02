86cef0053d78b466a60d30af2f5f1e43d027d69c
sauloal <sauloal@yahoo.com.br>
2015-09-02 15:31:49 +0200

    fixed bug in makefile

--- test2/data/Solanum_habrochaites_lyc4/get.sh
+++ test2/data/Solanum_habrochaites_lyc4/get.sh
@@ -1,2 +1,2 @@
-wget -q -O - ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR410/ERR410237/ERR410237_1.fastq.gz | pigz -dc | head -4000000 | pigz -1c > 1.fastq.gz
-wget -q -O - ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR410/ERR410237/ERR410237_2.fastq.gz | pigz -dc | head -4000000 | pigz -1c > 2.fastq.gz
+wget -q -O - ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR410/ERR410237/ERR410237_1.fastq.gz | pigz -dc | head -8000000 | pigz -1c > 1.fastq.gz
+wget -q -O - ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR410/ERR410237/ERR410237_2.fastq.gz | pigz -dc | head -8000000 | pigz -1c > 2.fastq.gz
