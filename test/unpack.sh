2a294be0c524af76bd44d357643874ff68f2661e
sauloal <sauloal@yahoo.com.br>
2015-09-29 22:30:30 +0200

    forgot to unpack

--- test/unpack.sh
+++ test/unpack.sh
@@ -2,5 +2,7 @@ set -xeu
 
 cd data
 
+tar axvf datafa.tar.xz
+
 find . -name *.fasta | xargs -I{} bash -c 'f={}; bn=`basename $f`; ap=`echo $f | sed "s/\.fasta//"`; echo $f $bn $ap; mkdir $ap; mv $f ap'
 
