008096382312f5855336b1dde56e1a97df055ff1
Saulo Aflitos <sauloal@gmail.com>
2016-08-24 18:41:43 +0200

    added static folder to ldpath

--- enable.sh
+++ enable.sh
@@ -1,2 +1,8 @@
 export PATH=$PWD/scripts:$PATH
 export PATH=$PWD/src/libs/Jellyfish/bin/:$PATH
+
+if [[ -z "${LD_LIBRARY_PATH}" ]]; then
+export LD_LIBRARY_PATH=$PWD/src/static
+else
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$PWD/src/static
+fi
